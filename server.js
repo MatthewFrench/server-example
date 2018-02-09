03f938c0711489475f04c112d604d249316b0f4b
MatthewFrench <matthewf@ksu.edu>
2018-02-09 15:16:27 -0600

    Completed the assignment

--- server.js
+++ server.js
@@ -9,40 +9,69 @@ const http = require('http');
 // Import the fs library 
 const fs = require('fs');
 
-const cache = {};
-cache['openhouse.html'] = fs.readFileSync('public/openhouse.html');
-cache['openhouse.css'] = fs.readFileSync('public/openhouse.css');
-cache['openhouse.js'] = fs.readFileSync('public/openhouse.js');
+// Import the path library
+const path = require('path');
 
-function serveIndex(path, res) {
-    fs.readdir(path, function(err, files) {
-        if(err) {
-            console.error(err);
-            res.statusCode = 500;
-            res.end("Server Error");
+function serveIndex(originalPath, requestedPath, res) {
+    fs.exists(path.join(requestedPath, 'index.html'), function(exists){
+        if (exists) {
+            res.writeHead(302,
+                {Location: path.join(originalPath, 'index.html')}
+            );
+            res.end();
+        } else {
+            fs.readdir(requestedPath, function(err, files) {
+                if(err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end("Server Error");
+                }
+                var html = "<p>Index of " + originalPath + "</p>";
+                html += "<ul>";
+                html += files.map(function(item){
+                    return "<li><a href='" + path.join(originalPath,item) + "'>" + item + "</a></li>";
+                }).join("");
+                html += "</ul>";
+                res.end(html);
+            });
         }
-        var html = "<p>Index of " + path + "</p>";
-        html += "<ul>";
-        html += files.map(function(item){
-            return "<li><a href='" + item + "'>" + item + "</a></li>";
-        }).join("");
-        html += "</ul>";
-        res.end(html);
     });
 }
 
 /** @function serveFile
  * Serves the specified file with the provided response object
- * @param {string} path - specifies the file path to read 
+ * @param {string} requestedPath - specifies the file requestedPath to read
  * @param {http.serverResponse} res - the http response object
  */
-function serveFile(path, res) {
-    fs.readFile(path, function(err, data) {
+function serveFile(requestedPath, res) {
+    fs.readFile(requestedPath, function(err, data) {
         if(err) {
           console.error(err);
           res.statusCode = 500;
           res.end("Server Error: Could not read file");
           return;
+        }
+        //Send back the mime type.
+        var extension = path.extname('requestedPath');
+        switch(extension) {
+            case 'html': {
+                res.setHeader('Content-Type', 'text/html');
+            } break;
+            case 'css': {
+                res.setHeader('Content-Type', 'text/css');
+            } break;
+            case 'js': {
+                res.setHeader('Content-Type', 'text/js');
+            } break;
+            case 'jpeg': {
+                res.setHeader('Content-Type', 'image/jpeg');
+            } break;
+            case 'png': {
+                res.setHeader('Content-Type', 'image/png');
+            } break;
+            case 'gif': {
+                res.setHeader('Content-Type', 'image/gif');
+            } break;
         }
         res.end(data);
     });
@@ -54,27 +83,25 @@ function serveFile(path, res) {
  * @param {http.ServerResponse} res - the http response object
  */
 function handleRequest(req, res) {
-    // Map request urls to files
-    switch(req.url) {
-        case '/':
-            serveIndex('public', res);
-            break;
-        case '/openhouse.html':
-            //res.end(cache['openhouse.html']);
-            serveFile('public/openhouse.html', res);
-            break;
-        case '/openhouse.css':
-            //res.end(cache['openhouse.css']);
-            serveFile('public/openhouse.css', res);
-            break;
-        case '/openhouse.js':
-            //res.end(cache['openhouse.js']);
-            serveFile('public/openhouse.js', res);
-            break;
-        default:
+    //Create requestedPath to the requested item
+    var originalPath = req.url;
+    var requestedPath = path.join('public', req.url);
+    fs.lstat(requestedPath, function (err, stats) {
+        if(err) {
             res.statusCode = 404;
             res.end("File Not Found");
-    }
+            return;
+        }
+        if (stats.isDirectory()) {
+            serveIndex(originalPath, requestedPath, res);
+        } else if (stats.isFile()) {
+            serveFile(requestedPath, res);
+        } else {
+            console.error(err);
+            res.statusCode = 500;
+            res.end("Server Error: Could not read file");
+        }
+    });
 }
 
 // Create the web server
